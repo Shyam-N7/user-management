fae8fa3e316389a7b9a164ec45711ade22c26751
Shyam-N7 <150006758+Shyam-N7@users.noreply.github.com>
2025-07-12 12:11:35 +0530

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -15,7 +15,7 @@ const LoginPage = () => {
         setError('');
         setLoading(true);
         try {
-            const response = await fetch('http://127.0.0.1:8000/login', {
+            const response = await fetch(https://user-management-wucu.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
